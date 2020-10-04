69ebc35d45fb50ccad2dbfdc1c03cea304b95da5
CodyECSL <ccoane@gmail.com>
2020-10-04 17:44:49 -0500

    Adding in Config

---
+++ src/config.h
@@ -0,0 +1,9 @@
+// Replace with your actual SSID and password:
+#define WIFI_SSID "WIFI_SSID_HERE"
+#define WIFI_PASSWD "WIFI_PW_HERE"
+
+// Your customized endpoint for RemoteHWInfo
+// Example: 
+// #define ENDPOINT_URL "http://10.10.183.64:60000/json.json?enable=176,180,183"
+#define ENDPOINT_URL "IP_PLUS_PORT_PLUS_IDS"
+
